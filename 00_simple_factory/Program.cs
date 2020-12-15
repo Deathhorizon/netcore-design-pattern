d7485f207d1761c8e73e7eb7a937149209e3edb7
王宇鹤 <2511980201@qq.com>
2020-12-15 17:00:19 +0800

    create

---
+++ 00_simple_factory/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _00_simple_factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IApi api = new Impl();
+            api.SayHi("hi");
+        }
+    }
+    public interface IApi
+    {
+        void SayHi(string s);
+    }
+    public class ImplA : IApi
+    {
+        public void SayHi(string s)
+        {
+            Console.WriteLine($"ImplA:{ s}");
+        }
+    }
+    public class ImplB : IApi
+    {
+        public void SayHi(string s)
+        {
+            Console.WriteLine($"ImplB:{ s}");
+        }
+    }
+    public class Factory
+    {
+        public void CreateApi()
+        {
+
+        }
+    }
+}
---
+++ 01_facade/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01_facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface IApi
+    {
+        void SayHi(string s);
+    }
+    public class ImplA : IApi
+    {
+        public void SayHi(string s)
+        {
+            Console.WriteLine($"ImplA:{ s}");
+        }
+    }
+    public class ImplB : IApi
+    {
+        public void SayHi(string s)
+        {
+            Console.WriteLine($"ImplB:{ s}");
+        }
+    }
+    public class Facade
+    {
+
+    }
+}
